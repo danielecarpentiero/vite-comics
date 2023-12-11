14c3b951ffe383ae9f44fdc8c561029810b32867
danielecarpentiero <carpentierodaniele@gmail.com>
2023-12-11 17:28:59 +0100

    feat: comics images

--- src/components/main/JumboMain.vue
+++ src/components/main/JumboMain.vue
@@ -1,24 +1,34 @@
 <template>
-        <h1 class="jumbotron">Content goes here</h1>
+  <div class="jumbotron">
+    <img :src="image" :alt="title" />
+  </div>
+  <SliderVue />
 </template>
 
 <script>
+import SliderVue from "./Slider.vue";
 export default {
-    name: 'JumboMain',
-    data() {
-        return {
-        };
-    },   
+  name: "JumboMain",
+  data() {
+    return {
+      image: "src/assets/img/jumbotron.jpg",
+      title: "teen-titans",
+    };
+  },
+  components: {
+    SliderVue,
+  },
 };
 </script>
 
 <style scoped lang="scss">
-@use 'src/styles/variables.scss' as *;
-    .jumbotron {
-        background-color: $secondary--color;
-        margin: 0;
-        color: $primary--color;
-        padding: 60px;
-        font-size: 2rem;
-    }
+@use "src/styles/variables.scss" as *;
+.jumbotron {
+  img {
+    width: 100%;
+    height: 400px;
+    object-fit: cover;
+    object-position: top;
+  }
+}
 </style>
---
+++ src/components/main/Slide.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <img :src="slide" alt="" />
+    <h3>{{ title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slide",
+  props: {
+    slide: String,
+    title: String,
+    genre: String,
+  },
+};
+</script>
+
+<style scoped lang="scss">
+@use "src/styles/variables.scss" as *;
+img {
+  width: 200px;
+  height: 200px;
+  object-fit: cover;
+  object-position: top;
+  display: block;
+}
+</style>
